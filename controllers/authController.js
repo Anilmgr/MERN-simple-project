f10534333de4333b9c147cb92b86fe4f120ffc40
Anilmgr <anilcodes60@gmail.com>
2024-07-12 01:38:09 +1000

    JWT token setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,26 @@
-import { StatusCodes } from 'http-status-codes';
-import User from '../models/UserModel.js'
-import { UnauthorizedError } from '../errors/customError.js';
-import { USER_ROLE } from '../utils/constants.js';
-import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
+import { StatusCodes } from "http-status-codes";
+import User from "../models/UserModel.js";
+import { UnauthorizedError } from "../errors/customError.js";
+import { USER_ROLE } from "../utils/constants.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
-export const register =  async (req,res) => {
-    const isFirstUser = await User.countDocuments() === 0
+export const register = async (req, res) => {
+    const isFirstUser = (await User.countDocuments()) === 0;
     req.body.role = isFirstUser ? USER_ROLE.ADMIN : USER_ROLE.USER;
-    req.body.password = await hashPassword(req.body.password)
+    req.body.password = await hashPassword(req.body.password);
     const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({message: "Account created successfully!"});
-}
+    res.status(StatusCodes.CREATED).json({
+        message: "Account created successfully!",
+    });
+};
 
-export const login = async (req,res)=>{
-    const user = await User.findOne({email:req.body.email});
-    const isValidUser = user && comparePassword(req.body.password, user.password);
-    if(!isValidUser) throw new UnauthorizedError('Email or password doesn\'t match!');
-    res.json({message:'Login Successfully!'})
-}
+export const login = async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    const isValidUser =
+        user && comparePassword(req.body.password, user.password);
+    if (!isValidUser)
+        throw new UnauthorizedError("Email or password doesn't match!");
+    const token = createJWT({ userId: user._id, role: user.role });
+    res.json({ token });
+};
