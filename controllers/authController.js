14d92434c4ed75f0af10b319b6c8ddc201a0bbbe
Anil Baral <anil@anils-mbp.lan>
2024-07-11 15:52:10 +1000

    Authentication & hash

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,20 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js'
 import { UnauthorizedError } from '../errors/customError.js';
+import { USER_ROLE } from '../utils/constants.js';
+import { comparePassword, hashPassword } from '../utils/passwordUtils.js';
 
 export const register =  async (req,res) => {
+    const isFirstUser = await User.countDocuments() === 0
+    req.body.role = isFirstUser ? USER_ROLE.ADMIN : USER_ROLE.USER;
+    req.body.password = await hashPassword(req.body.password)
     const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({user});
+    res.status(StatusCodes.CREATED).json({message: "Account created successfully!"});
 }
 
 export const login = async (req,res)=>{
-    const user = await User.find(req.body);
-    if(!user) throw new UnauthorizedError('Email or password doesn\'t match!');
-
+    const user = await User.findOne({email:req.body.email});
+    const isValidUser = user && comparePassword(req.body.password, user.password);
+    if(!isValidUser) throw new UnauthorizedError('Email or password doesn\'t match!');
+    res.json({message:'Login Successfully!'})
 }
--- middleware/validators.js
+++ middleware/validators.js
@@ -3,6 +3,7 @@ import { BadRequestError, NotFoundError } from "../errors/customError.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
 import Job from "../models/JobModel.js";
+import User from "../models/UserModel.js";
 
 const withValidationErrors = (validateValues) => {
     return [
@@ -24,7 +25,7 @@ const withValidationErrors = (validateValues) => {
 export const validateJobInput = withValidationErrors([
     body("company").notEmpty().withMessage("Company is required!"),
     body("position").notEmpty().withMessage("Position is required!"),
-    body("jobLocation").notEmpty().withMessage("Position is required!"),
+    body("jobLocation").notEmpty().withMessage("Location is required!"),
     body("jobStatus")
         .isIn(Object.values(JOB_STATUS))
         .withMessage("invalid job status!"),
@@ -36,8 +37,41 @@ export const validateJobInput = withValidationErrors([
 export const validateIdParam = withValidationErrors([
     param("id").custom(async (value) => {
         const isValidId = mongoose.Types.ObjectId.isValid(value);
-        if (!isValidId) throw new  ("Invalid MongoDB ID!");
+        if (!isValidId) throw new "Invalid MongoDB ID!"();
         const job = await Job.findById(value);
         if (!job) throw new NotFoundError("Job not found!");
     }),
+]);
+
+export const validateUserInput = withValidationErrors([
+    body("name").notEmpty().withMessage("Name is required!"),
+    body("email")
+        .notEmpty()
+        .withMessage("Email is required!")
+        .isEmail()
+        .withMessage("Invalid email format")
+        .custom(async (email) => {
+            const user = await User.findOne({ email });
+            if (user) throw new BadRequestError("Email already exist!");
+        }),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required!")
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 character!"),
+    body("location").notEmpty().withMessage("Location is required!"),
+    body("lastName").notEmpty().withMessage("Last Name is required!"),
+]);
+
+export const validateLoginInput = withValidationErrors([
+    body("email")
+        .notEmpty()
+        .withMessage("Email is required!")
+        .isEmail()
+        .withMessage("Invalid email format"),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required!")
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 character!"),
 ]);
