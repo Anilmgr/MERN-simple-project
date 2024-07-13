d23db0679350c579848c7525bea25607a3eb3407
Anilmgr <anilcodes60@gmail.com>
2024-07-13 13:24:26 +1000

    Wrap up auth

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,12 +1,14 @@
+import { NotFoundError } from "../errors/customError.js";
 import Job from "../models/JobModel.js";
 import { StatusCodes } from "http-status-codes";
 
 export const getAllJobs = async (req, res) => {
-    const jobs = await Job.find();
+    const jobs = await Job.find({createdBy: req.user.userId});
     res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
+    req.body.createdBy = req.user.userId;
     const job = await Job.create(req.body);
     res.status(StatusCodes.CREATED).json({ job });
 };
@@ -20,7 +22,8 @@ export const deleteJob = async (req, res) => {
 };
 
 export const getSingleJob = async (req, res) => {
-    const job = await Job.findById(req.params.id);
+    const job = await Job.findOne({_id:req.params.id, createdBy:req.user.userId});
+    if (!job) throw new NotFoundError("Job not found!");
     res.status(StatusCodes.OK).json(job);
 };
 
