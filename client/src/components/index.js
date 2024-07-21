71749019e5c662d09750476fbc79efb4503fb413
Anilmgr <anilcodes60@gmail.com>
2024-07-21 13:41:35 +1000

    Add job page done

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Logo} from './Logo';
 export { default as NotFoundImg} from './NotFoundImg';
 export { default as FormRow} from './FormRow';
+export { default as FormRowSelect} from './FormRowSelect';
 export { default as BigSidebar} from './BigSidebar';
 export { default as SmallSidebar} from './SmallSidebar';
 export { default as Navbar} from './Navbar';
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,7 +1,37 @@
-const AddJob = () => {
-  return (
-    <div>AddJob</div>
-  )
-}
+import { Form, useNavigation, useOutletContext } from "react-router-dom";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { FormRow, FormRowSelect } from "../components";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
 
-export default AddJob
+const AddJob = () => {
+    const { user } = useOutletContext();
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === "submitting";
+    return (
+        <Wrapper>
+            <Form method="post" className="form">
+                <h4 className="form-title">Add Job</h4>
+                <div className="form-center">
+                    <FormRow type="text" name="position" />
+                    <FormRow type="text" name="company" />
+                    <FormRow
+                        type="text"
+                        labelText="Job Location"
+                        name="jobLocation"
+                    />
+                    <FormRowSelect name="jobStatus" labelText="Job Status" list={Object.values(JOB_STATUS)}/>
+                    <FormRowSelect name="jobType" labelText="Job Type" list={Object.values(JOB_TYPE)}/>
+                    <button
+                        type="submit"
+                        className="btn btn-block form-btn"
+                        disabled={isSubmitting}
+                    >
+                        {isSubmitting ? "Submitting..." : "Submit"}
+                    </button>
+                </div>
+            </Form>
+        </Wrapper>
+    );
+};
+
+export default AddJob;
