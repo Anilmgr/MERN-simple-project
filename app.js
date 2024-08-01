269d2cab1f78e77b613dc62e63af47d3d8296aea
Anilmgr <anilcodes60@gmail.com>
2024-08-01 15:49:48 +1000

    Cloudnary done

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-import 'express-async-errors';
+import "express-async-errors";
 import express from "express";
 import "dotenv/config";
 import morgan from "morgan";
-import jobRouter from './routes/jobRouter.js';
-import authRouter from './routes/authRouter.js';
-import userRouter from './routes/userRouter.js';
+import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
 import mongoose from "mongoose";
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-import {authenticatedUser} from './middleware/authMiddleware.js';
-import cookieParser from 'cookie-parser';
-import path, {dirname} from 'path';
-import { fileURLToPath } from 'url';
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticatedUser } from "./middleware/authMiddleware.js";
+import cookieParser from "cookie-parser";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { v2 as cloudinary } from "cloudinary";
 
 let app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url))
-app.use(express.static(path.resolve(__dirname,'./public')))
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
-// Routers
-app.use("/api/v1/jobs",authenticatedUser, jobRouter);
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET, // Click 'View Credentials' below to copy your API secret
+});
 
-app.use("/api/v1/test",(req,res)=>{
-    res.json({message:"Hello World!"})
+// Routers
+app.use("/api/v1/jobs", authenticatedUser, jobRouter);
+
+app.use("/api/v1/test", (req, res) => {
+    res.json({ message: "Hello World!" });
 });
 
 app.use("/api/v1/users", authenticatedUser, userRouter);
@@ -38,12 +45,11 @@ app.use("*", (req, res) => {
     res.status(404).json({ message: "Requested uri not found" });
 });
 
-app.use(errorHandlerMiddleware)
-
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 3000;
 try {
-    await mongoose.connect(process.env.CONNECT_URI)
+    await mongoose.connect(process.env.CONNECT_URI);
     app.listen(PORT, () => {
         console.log(`Server running on PORT ${PORT}!`);
     });
@@ -51,4 +57,3 @@ try {
     console.log(error);
     process.exit(1);
 }
-
